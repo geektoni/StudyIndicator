e4981ba87b7429a9c6da039ea465697449e882de
Giovanni De Toni <giovanni.det@gmail.com>
2016-02-08 12:51:47 +0100

    Populate the Timer class
    Add setter and getter, also add function to set elapsed time.

--- Timer.py
+++ Timer.py
@@ -1,5 +1,37 @@
+# Main library
+import time
+
 class Timer(object):
     """ Timer class """
 
     def __init__(self):
-        pass
+        self.startTime = 0
+        self.phaseDuration = 25
+        self.breakDuration = 5
+        self.sessionDuration = self.phaseDuration*4+self.breakDuration*4
+
+    def setPhaseDuration(duration):
+        self.phaseDuration = duration
+
+    def getPhaseDuration():
+        return self.phaseDuration
+
+    def setBreakDuration(duration):
+        self.breakDuration = duration
+
+    def getBreakDuration():
+        return self.breakDuration
+
+    def setSessionDuration(duration):
+        self.sessionDuration = duration
+
+    def getSessionDuration():
+        return self.sessionDuration
+
+    def setStartTime():
+        self.startTimer = time.mktime(time.localtime(time.time())
+
+    def getElapsedTime():
+        seconds = int(time.mktime(time.localtime(time.time())) - self.startTime)
+        minutes = int(seconds/60)
+        return (minutes, seconds)
